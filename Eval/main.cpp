5b3085cf8c7789de21a6bd394fbff9a239165fef
Tyler Harbert <harbertt11@gmail.com>
2015-09-25 11:35:00 -0400

    initial cmake commit

---
+++ Eval/main.cpp
@@ -0,0 +1,25 @@
+// Tyler Harbert
+
+#include <iostream>
+#include "parser.hpp"
+
+using namespace std;
+
+int main()
+{
+    // create a parser class
+    Parser* parser = new Parser();
+    // create an AST using the parser
+    Expr* expression = parser->E();
+    // create and evaluator class
+    Eval eval;
+
+    // pass the evaluator class to the
+    // root node of the AST's accept function
+    expression->accept(eval);
+
+    // print result
+    eval->result;
+
+    return 0;
+}
